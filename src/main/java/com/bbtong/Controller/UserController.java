0a654f95c0c8b51bb5eae5475afb86acdc823155
DaiLiang <1764884544@qq.com>
2020-06-03 10:11:34 +0800

    1.修改提交

--- src/main/java/com/bbtong/Controller/UserController.java
+++ src/main/java/com/bbtong/Controller/UserController.java
@@ -71,30 +71,38 @@ public class UserController {
         //创建Result的实体来接受数据
         UserResult userResult = new UserResult();
         //随机生成六位随机数
-        int num = (int) ((Math.random() * 9 + 1) * 100000);
+        int num = (int) ((Math.random() * 9 + 1) * 1000);
         //将数据存到newUser的实体中，然后将数据存到session中
 
         //将newUser存到session中，用于登陆的判断
         session.setAttribute(userPhone, num);
         //设置session的过期时间，这是设置成5分钟
-        session.setMaxInactiveInterval(5);
+        session.setMaxInactiveInterval(30 * 60);
 
-//       userResult=sms.getRequest2(userPhone,num);
+        //userResult = sms.getRequest2(userPhone, num);
         return userResult;
     }
 
+    /**
+     * 验证用户短信和手机号，是否一致的方法，如果一致的话就登陆成功将数据存到cookie中
+     *
+     * @param userPhone 手机号码
+     * @param num       验证码
+     * @param session   用来接受存储数据
+     * @return 戴辆
+     */
     @GetMapping(value = "/login", produces = "application/json")
     public @ResponseBody
     UserResult UserLogin(String userPhone, int num, HttpSession session) {
         //创建实体类Result来接受数据
         UserResult userResult = new UserResult();
-//        //创建request实体来接受和操作session数据
+        //创建request实体来接受和操作session数据
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-//        //创建session接受数据
+        //创建session接受数据
         session = request.getSession();
         //创建NewUser实体来判断数据是否一致
         Enumeration<String> attrs = session.getAttributeNames();
-        String newPhone=attrs.nextElement().toString();
+        String newPhone = attrs.nextElement().toString();
         if (!userPhone.equals(newPhone)) {
             userResult.setCode(300);
             userResult.setMessage("手机号码错误");
@@ -105,6 +113,8 @@ public class UserController {
             userResult.setMessage("验证码错误");
             return userResult;
         }
+        //登陆成功之后将seesion给清除掉
+        session.removeAttribute(newPhone);
         //第一步先根据手机号，去查询有没有注册账号
         userResult = userService.PutUser(userPhone);
         return userResult;
